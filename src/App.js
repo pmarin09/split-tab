9dfcfc8a8ac43d90da2f18aa4d62f5f545c78310
Patrick <patrickmarin@gmail.com>
2020-03-17 19:48:14 -0500

    checkboxes

--- src/App.js
+++ src/App.js
@@ -7,65 +7,26 @@ import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 import './App.css';
 
 
-firebase.initializeApp({
-  apiKey: "AIzaSyAYzeOmKOk5adpLs9VF4CkpSFK-wZTESac",
-  authDomain: "splittab-fb51c.firebaseapp.com"
-})
-
 function App() {
-  const [isSignedIn, setIsSignedIn] = useState(false)
+  const [checkBox, setcheckBox] = useState(true)
+  const [checkedMsg, setcheckedMsg] = useState("")
   const tabItems = tabData.map(item => <TabItem key={item.id} item={item}/>)
-  const uiConfig = {
-    signInFlow: "popup",
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID
-    ],
-    callbacks: {
-      signInSuccessWithAuthResult: () => false
-    }
-  }
-  console.log(firebase)
-  //useEffect(()=> {
 
-    // firebase.auth().onAuthStateChanged( (user) =>  
-    //   setIsSignedIn([!!isSignedIn])
-     // console.log(true)
-    // )
-  //}, [])
 
-  function signOut() {
-    firebase.auth().signOut().then(function() {
-      // Sign-out successful.
-    }).catch(function(error) {
-      // An error happened.
-    });
+  useEffect(()=> {
+    checkBox ? setcheckedMsg({ checkedMsg: 'HOLA' }) : setcheckedMsg({ checkedMsg: 'unchecked' })
+  function tick() {
+    setcheckBox({checkBox: !checkBox})
   }
+    }, [])
 return(
-      // <div className="tab-list">
-      //   <h1 className = "title">Welcome to SplitTab</h1>
-        
-      //    {isSignedIn ? (
-      //     <span>
-      //       <button onClick={signOut}>Sign out!</button>
-      //       <h1>Hi {firebase.auth().currentUser}</h1>
-      //       {tabItems}
-      //     </span>
-      //   ) : (
-      //     <StyledFirebaseAuth
-      //       uiConfig={uiConfig}
-      //       firebaseAuth={firebase.auth()}
-      //     />)   
-      //   }
-      //   </div>
         <div>
             <div className="pricing-table">
-            <div className="tab-heading"></div>
-            
-            <div className= "tab-info">
-            {tabItems}	
-              </div>		
-                	
+              <div className="tab-heading"></div>
+              <div className= "tab-info">
+                {tabItems}	
+                
+              </div>			
           </div>
         </div>     
       )
@@ -73,3 +34,5 @@ return(
   }
 
 export default App;
+
+
--- src/tabItem.js
+++ src/tabItem.js
@@ -1,13 +1,21 @@
 import React from "react"
 import style from "./style.css"
 function TabItem(props) {
+   
     return (
         <div className="tab-item">
             <input 
-            type="checkbox" checked={props.item.checked}
+            type="checkbox" 
+            onClick={props.tick}
+            defaultChecked={props.check}
+            // checked={props.item.checked}
             />
+            <h1>{props.checkedMsg}</h1>
             <p>{props.item.text} - ${props.item.price}</p>
+            
         </div>
     )
 }
+
+
 export default TabItem

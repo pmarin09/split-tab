612a939f36b6922d7a7e15b741df4b78ed241a2f
Patrick <patrickmarin@gmail.com>
2020-03-20 18:07:56 -0500

    Total Calculation

--- src/total.js
+++ src/total.js
@@ -3,24 +3,31 @@ import './buttons.css';
 import TabItem from "./tabItem.js"
 function Total(props){
     const [tax, setTax] = useState(0);
-    const [tip, setTip] = useState(0);
+    let [tip, setTip] = useState(0);
+    let tabtot = (parseFloat(props.subtotal) + parseFloat(tax) + parseFloat(tip)).toFixed(2)
+
     useEffect(
-    
+        
         ()=>setTax((Number.parseFloat(props.subtotal)*0.07).toFixed(2)), [props.subtotal],
-        ()=>setTip((Number.parseFloat(props.subtotal)*0.10).toFixed(2)), [props.subtotal]
     )
+    
+    // function tipCalc(){
+    //    tip = ((parseFloat(props.subtotal)*.10))
+    //     console.log(tip)
+    // }
+      
     
 
     return(
      <div className="grid-total-container">
          <div className="grid-item"><h4 className="total">Subtotal:${props.subtotal}</h4></div>
          <div className="grid-item"><h4 className="total">Tax:${tax}</h4></div>
-         <div className="grid-tip-container"><h4 className="total">Tip:</h4>
-            <div><button type="button" className="btn btn-outline-info btn-xs btn-radius">{tip}</button></div>
-            <div><button type="button" className="btn btn-outline-info btn-xs btn-radius">15%</button></div>
-            <div><button type="button" className="btn btn-outline-info btn-xs btn-radius">20%</button></div>
+         <div className="grid-tip-container"><h4 className="total">Tip: ${tip}</h4>
+            <div><button type="button" className="btn btn-outline-info btn-xs btn-radius">10%</button></div>
+            <div><button type="button" className="btn btn-outline-info btn-xs btn-radius" >15%</button></div>
+            <div><button type="button" className="btn btn-outline-info btn-xs btn-radius" >20%</button></div>
         </div>
-        <div className="grid-item"><h3 className="total">Total:</h3></div>
+        <div className="grid-item"><h3 className="total">Total:${tabtot}</h3></div>
     </div>
     )
 }

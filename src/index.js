2b9493722e93fcfd1c96782069d04517e3e50be8
Patrick <patrickmarin@gmail.com>
2020-03-25 18:31:40 -0500

    ws config

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-var wss = new WebSocketServer({port: process.env.PORT || port});
+const ws = new WebSocket('ws:https://splittab-ws.herokuapp.com/');
 
 ws.onopen = () => {
   console.log('websocket is connected ...');
